15d71a3262f13f0443c59d1f82b0dd3b1251e158
Markus Scheich <markus.scheich@gmx.de>
2018-07-17 21:55:07 +0200

    Initial commit

---
+++ AssemblyInfoHelper/AssemblyKnownIssuesAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyInfoHelper
+{
+    /// <summary>
+    /// Attribute to report known issues/bugs
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+    public class AssemblyKnownIssuesAttribute : Attribute
+    {
+        /// <summary>
+        /// Text describing the issue
+        /// </summary>
+        public string IssueText { get; set; }
+
+        /// <summary>
+        /// Constructor of the AssemblyKnownIssuesAttribute
+        /// </summary>
+        /// <param name="issueText">Text describing the issue</param>
+        public AssemblyKnownIssuesAttribute(string issueText)
+        {
+            IssueText = issueText;
+        }
+
+    }
+}
