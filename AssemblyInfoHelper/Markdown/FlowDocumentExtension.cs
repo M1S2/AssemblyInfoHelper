5cddea888a737c17a05f26580a0a014fa37971c1
Markus Scheich <markus.scheich@gmx.de>
2019-11-05 08:48:23 +0100

    MarkdownViewer loading document in async thread tried (hyperlinks disabled when loaded in separate thread ?)

---
+++ AssemblyInfoHelper/Markdown/FlowDocumentExtension.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+using System.Reflection;
+using System.Collections;
+using System.Windows.Documents;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace AssemblyInfoHelper.Markdown
+{
+    public static class FlowDocumentExtension
+    {
+        //public static void EnableAllHyperlinks(this FlowDocument document)
+        //{
+        //    //FieldInfo field = typeof(System.Windows.ContentElement).GetField("_isEnabledCore", BindingFlags.NonPublic | BindingFlags.Instance);
+        //    //PropertyInfo property = typeof(System.Windows.ContentElement).GetProperty("IsEnabledCore");
+
+        //    //see: https://www.productiverage.com/trying-to-set-a-readonly-autoproperty-value-externally-plus-a-little-benchmarkdotnet
+        //    var type = typeof(System.Windows.ContentElement);
+        //    var property = type.GetProperty("IsEnabled");
+
+        //    var backingField = type
+        //      .GetFields(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static)
+        //      .FirstOrDefault(field =>
+        //        field.Attributes.HasFlag(FieldAttributes.Private) &&
+        //        field.Attributes.HasFlag(FieldAttributes.InitOnly) &&
+        //        field.CustomAttributes.Any(attr => attr.AttributeType == typeof(CompilerGeneratedAttribute)) &&
+        //        (field.DeclaringType == property.DeclaringType) &&
+        //        field.FieldType.IsAssignableFrom(property.PropertyType) &&
+        //        field.Name.StartsWith("<" + property.Name + ">")
+        //      );
+
+        //    EnableHyperlink(document, property, FlowDocumentVisitors);
+        //}
+
+        /// <summary>
+        /// Attach the FlowDocument to the current thread. The dispatcher of all FlowDocument elements are changed to the dispatcher of the current thread.
+        /// This needs to be done to avoid the "The calling thread cannot access this object because a different thread owns it" error.
+        /// </summary>
+        /// <param name="document">The FlowDocument to attach</param>
+        /// see: https://turecki.net/flowdocument-from-a-different-thread
+        public static void AttachToCurrentThread(this FlowDocument document)
+        {
+            Dispatcher dispatcher = Dispatcher.CurrentDispatcher;
+            FieldInfo field = typeof(DispatcherObject).GetField("_dispatcher", BindingFlags.NonPublic | BindingFlags.Instance);
+            if (field == null)
+            {
+                throw new InvalidOperationException("_dispatcher field missing on DispatcherObject");
+            }
+            SetDispatcher(document, field, dispatcher, FlowDocumentVisitors);
+
+            PropertyInfo property = typeof(ContentElement).GetProperty("IsEnabled");
+            if (property == null)
+            {
+                throw new InvalidOperationException("IsEnabled property missing on ContentElement");
+            }
+            EnableAllElements(document, property, FlowDocumentVisitors);
+        }
+
+        private static readonly Func<object, object>[] FlowDocumentVisitors =
+        {
+            x => (x is FlowDocument) ? ((FlowDocument) x).Blocks : null,
+            x => (x is Section) ? ((Section) x).Blocks : null,
+            x => (x is BlockUIContainer) ? ((BlockUIContainer) x).Child : null,
+            x => (x is InlineUIContainer) ? ((InlineUIContainer) x).Child : null,
+            x => (x is Span) ? ((Span) x).Inlines : null,
+            x => (x is AnchoredBlock) ? ((AnchoredBlock) x).Blocks : null,
+            x => (x is Paragraph) ? ((Paragraph) x).Inlines : null,
+            x => (x is Table) ? ((Table) x).RowGroups : null,
+            x => (x is Table) ? ((Table) x).Columns : null,
+            x => (x is Table) ? ((Table) x).RowGroups.SelectMany(rg => rg.Rows) : null,
+            x => (x is Table) ? ((Table) x).RowGroups.SelectMany(rg => rg.Rows).SelectMany(r => r.Cells) : null,
+            x => (x is TableCell) ? ((TableCell) x).Blocks : null,
+            x => (x is TableCell) ? ((TableCell) x).BorderBrush : null,
+            x => (x is List) ? ((List) x).ListItems : null,
+            x => (x is ListItem) ? ((ListItem) x).Blocks : null
+        };
+
+        private static void SetDispatcher(object item, FieldInfo field, object value, params Func<object, object>[] selectors)
+        {
+            if (item is DispatcherObject)
+            {
+                Dispatcher currentDispatcher = field.GetValue(item) as Dispatcher;
+                if (currentDispatcher != null && currentDispatcher != value)
+                {
+                    field.SetValue(item, value);
+                }
+            }
+            if (item is IEnumerable)
+            {
+                foreach (var subItem in item as IEnumerable)
+                {
+                    SetDispatcher(subItem, field, value, selectors);
+                }
+            }
+            if (selectors != null)
+            {
+                foreach (var selector in selectors.Select(x => x(item)).Where(x => x != null))
+                {
+                    SetDispatcher(selector, field, value, selectors);
+                }
+            }
+        }
+
+        private static void EnableAllElements(object item, PropertyInfo property, params Func<object, object>[] selectors)
+        {
+            if (item is ContentElement)
+            {
+                property.SetValue(item, true);
+            }
+            if (item is IEnumerable)
+            {
+                foreach (var subItem in item as IEnumerable)
+                {
+                    EnableAllElements(subItem, property, selectors);
+                }
+            }
+            if (selectors != null)
+            {
+                foreach (var selector in selectors.Select(x => x(item)).Where(x => x != null))
+                {
+                    EnableAllElements(selector, property, selectors);
+                }
+            }
+        }
+    }
+}
--- AssemblyInfoHelper/Markdown/MarkdownViewer.xaml.cs
+++ AssemblyInfoHelper/Markdown/MarkdownViewer.xaml.cs
@@ -36,6 +36,15 @@ namespace AssemblyInfoHelper.Markdown
         }
         public static readonly DependencyProperty MarkdownStringProperty = DependencyProperty.Register("MarkdownString", typeof(string), typeof(MarkdownViewer), new PropertyMetadata("", MarkdownStringChanged));
 
+
+        public bool DocumentIsLoading
+        {
+            get { return (bool)this.GetValue(DocumentIsLoadingProperty); }
+            private set { this.SetValue(DocumentIsLoadingProperty, value); }
+        }
+        public static readonly DependencyProperty DocumentIsLoadingProperty = DependencyProperty.Register("DocumentIsLoading", typeof(bool), typeof(MarkdownViewer), new PropertyMetadata(false));
+
+
         public MarkdownViewer()
         {
             InitializeComponent();
@@ -48,16 +57,147 @@ namespace AssemblyInfoHelper.Markdown
         /// </summary>
         /// see: https://stackoverflow.com/questions/5579415/wpf-richtextbox-document-creation-threading-issue
         /// see: https://turecki.net/flowdocument-from-a-different-thread
-        private static void MarkdownStringChanged(object sender, DependencyPropertyChangedEventArgs e)
+        private async static void MarkdownStringChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
             MarkdownViewer viewer = (MarkdownViewer)sender;
 
             string markdownString = viewer.MarkdownString;
             if (markdownString == null) { return; }
 
-            MarkdownPipeline pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
-            FlowDocument doc = MarkdownXaml.ToFlowDocument(markdownString, pipeline);
+            viewer.DocumentIsLoading = true;
+
+            FlowDocument doc = new FlowDocument();
+            UIElement loadingTemplate = (UIElement)viewer.FindResource("LoadingTemplate");
+            doc.Blocks.Add(new BlockUIContainer(loadingTemplate));
             viewer.Document = doc;
+
+            //doc.Blocks.Add(new Paragraph(new Hyperlink(new Run("Link"))));
+            //viewer.Document = doc;
+
+            MarkdownPipeline pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
+            SemaphoreSlim syncEvent = new SemaphoreSlim(0,1);
+            Thread thread = new Thread(() => 
+            {
+                doc = MarkdownXaml.ToFlowDocument(markdownString, pipeline);
+                //doc.Blocks.Add(new BlockUIContainer(new Button() { Content = "ABC" }));
+                //string xaml = MarkdownXaml.ToXaml(markdownString, pipeline);
+                syncEvent.Release();
+            });
+            thread.SetApartmentState(ApartmentState.STA); //Set the thread to STA
+            thread.Start();
+            thread.IsBackground = true;
+            await syncEvent.WaitAsync();
+
+            //foreach (Block block in doc.Blocks)
+            //{
+            //    List<Hyperlink> hyperLinks = block.FindChildren<Hyperlink>().ToList();
+            //}
+            //List<Image> images = FindImages(doc).ToList();
+
+#warning Hyperlink is disabled when the document is created in new Thread (without the thread, the links are enabled)
+#warning Freezable object, HostVisual ?
+#warning Even if the document is created without an extra thread, the IsEnabled and IsEnabledCore properties of the Hyperlink are False (and the link works) ???!!!
+
+            doc.AttachToCurrentThread();
+            //doc.EnableAllHyperlinks();
+            //doc.IsEnabled = true;
+            viewer.Document = doc;
+            //viewer.Document.Blocks.Clear();
+            //viewer.Document.Blocks.AddRange(doc.Blocks);
+            viewer.DocumentIsLoading = false;
+        }
+
+
+
+
+
+        //********************************************************************************************************************************************************************
+
+        //https://blogs.msdn.microsoft.com/dwayneneed/2007/04/26/multithreaded-ui-hostvisual/
+        //https://stackoverflow.com/questions/11358647/how-to-access-separate-thread-generated-wpf-ui-elements-from-the-dispatcher-thre
+
+        private async static void MarkdownStringChanged2(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            MarkdownViewer viewer = (MarkdownViewer)sender;
+
+            string markdownString = viewer.MarkdownString;
+            if (markdownString == null) { return; }
+
+            viewer.DocumentIsLoading = true;
+
+            FlowDocument doc = new FlowDocument();
+            UIElement loadingTemplate = (UIElement)viewer.FindResource("LoadingTemplate");
+            doc.Blocks.Add(new BlockUIContainer(loadingTemplate));
+            viewer.Document = doc;
+
+#warning Test
+            ResourceKey key = MarkdownXaml.CodeBlockStyleKey;
+            MarkdownPipeline pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
+
+
+            viewer.CreateElementOnSeperateThread2(() =>
+            {
+                //return new FlowDocument(); 
+                return new FlowDocument(new Paragraph(new Run("Hallo")));
+                //return MarkdownXaml.ToFlowDocument(markdownString, pipeline);
+            }, 
+            result =>
+            {
+                viewer.Document = (FlowDocument)result;
+                viewer.DocumentIsLoading = false;
+            });
+
+            /*viewer.CreateElementOnSeperateThread2(() =>
+            {
+                return new TextBlock(new Run("ABC"));
+            },
+            result =>
+            {
+                doc.Blocks.Add(new BlockUIContainer((UIElement)result));
+                viewer.Document = doc;
+            });*/
+
+
+            //HostVisual hostVisual = new HostVisual();
+
+            //            // Spin up a worker thread, and pass it the HostVisual that it should be part of.
+            //            AutoResetEvent s_event = new AutoResetEvent(false);
+            //            Thread thread = new Thread(new ParameterizedThreadStart((arg) => 
+            //            {
+            //#warning ...
+            //            }));
+            //            thread.SetApartmentState(ApartmentState.STA);
+            //            thread.IsBackground = true;
+            //            thread.Start(hostVisual);
+
+            //            // Wait for the worker thread to spin up and create the VisualTarget.
+            //            s_event.WaitOne();
+            //            return hostVisual;
+
+
+            //System.Threading.SemaphoreSlim syncEvent = new System.Threading.SemaphoreSlim(0, 1);
+            //System.Threading.Thread thread = new System.Threading.Thread(() =>
+            //{
+            //    doc = MarkdownXaml.ToFlowDocument(markdownString, new MarkdownPipelineBuilder().UseAdvancedExtensions().Build());
+            //    //string xaml = MarkdownXaml.ToXaml(markdownString, new MarkdownPipelineBuilder().UseAdvancedExtensions().Build());
+            //    syncEvent.Release();
+            //});
+            //thread.SetApartmentState(System.Threading.ApartmentState.STA); //Set the thread to STA
+            //thread.Start();
+            //thread.IsBackground = true;
+            //await syncEvent.WaitAsync();
+
+
+#warning Hyperlink is disabled when the document is created in new Thread (without the thread, the links are enabled)
+#warning Freezable object, HostVisual ?
+
+            //doc.AttachToCurrentThread();
+            //doc.EnableAllHyperlinks();
+            //doc.IsEnabled = true;
+            //viewer.Document = doc;
+            //viewer.Document.Blocks.Clear();
+            //viewer.Document.Blocks.AddRange(doc.Blocks);
+            //viewer.DocumentIsLoading = false;
         }
 
         //********************************************************************************************************************************************************************
@@ -74,5 +214,145 @@ namespace AssemblyInfoHelper.Markdown
             }
         }
 
+
+        /// <summary>
+        /// Creates UI element on a seperate thread and transfers it to
+        /// main UI thread. 
+        /// 
+        /// Usage; if you have complex UI operation that takes a lot of time, such as XPS object creation.
+        /// </summary>
+        /// <param name="constructObject">Function that creates the necessary DependencyObject - will be executed on new thread</param>
+        /// <param name="constructionCompleted">Callback to the function that receives the constructed object.</param>
+        /// see: https://stackoverflow.com/questions/11358647/how-to-access-separate-thread-generated-wpf-ui-elements-from-the-dispatcher-thre
+        public void CreateElementOnSeperateThread(Func<DependencyObject> constructObject, Action<DependencyObject> constructionCompleted)
+        {
+            VerifyAccess();
+
+            // save dispatchers for future usage.
+            // we create new element on a seperate STA thread
+            // and then basically swap UIELEMENT's Dispatcher.
+            Dispatcher threadDispatcher = null;
+            Dispatcher currentDispatcher = Dispatcher.CurrentDispatcher;
+
+            AutoResetEvent ev = new AutoResetEvent(false);
+            Thread thread = new Thread(() =>
+            {
+                threadDispatcher = Dispatcher.CurrentDispatcher;
+                ev.Set();
+
+                Dispatcher.Run();
+            });
+
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.IsBackground = true;
+            thread.Start();
+
+            ev.WaitOne();
+
+            threadDispatcher.BeginInvoke(new Action(() =>
+            {
+                DependencyObject constructedObject = constructObject();
+                currentDispatcher.BeginInvoke(new Action(() =>
+                {
+                    if (constructedObject.GetType() == typeof(FlowDocument))
+                    {
+                        ((FlowDocument)constructedObject).AttachToCurrentThread();
+                    }
+                    else
+                    {
+                        FieldInfo fieldinfo = typeof(DispatcherObject).GetField("_dispatcher", BindingFlags.NonPublic | BindingFlags.Instance);
+                        if (fieldinfo != null) { fieldinfo.SetValue(constructedObject, currentDispatcher); }
+                    }
+
+                    constructionCompleted(constructedObject);
+                    threadDispatcher.BeginInvokeShutdown(DispatcherPriority.Normal);
+                }), DispatcherPriority.Normal);
+            }), DispatcherPriority.Normal);
+        }
+
+
+
+        public void CreateElementOnSeperateThread2(Func<DependencyObject> constructObject, Action<DependencyObject> constructionCompleted)
+        {
+            VerifyAccess();
+
+            // save dispatchers for future usage.
+            // we create new element on a seperate STA thread
+            // and then basically swap UIELEMENT's Dispatcher.
+            Dispatcher threadDispatcher = null;
+            Dispatcher currentDispatcher = Dispatcher.CurrentDispatcher;
+
+            AutoResetEvent ev = new AutoResetEvent(false);
+            Thread thread = new Thread(() =>
+            {
+                threadDispatcher = Dispatcher.CurrentDispatcher;
+                DependencyObject constructedObject = constructObject();
+                currentDispatcher.BeginInvoke(new Action(() =>
+                {
+                    if (constructedObject.GetType() == typeof(FlowDocument))
+                    {
+                        ((FlowDocument)constructedObject).AttachToCurrentThread();
+                    }
+                    else
+                    {
+                        FieldInfo fieldinfo = typeof(DispatcherObject).GetField("_dispatcher", BindingFlags.NonPublic | BindingFlags.Instance);
+                        if (fieldinfo != null) { fieldinfo.SetValue(constructedObject, currentDispatcher); }
+                    }
+
+                    constructionCompleted(constructedObject);
+                }), DispatcherPriority.Normal);
+
+                ev.Set();
+            });
+
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.IsBackground = true;
+            thread.Start();
+
+            ev.WaitOne();
+        }
+
+
+        #region Not used
+        /*
+        //see: https://stackoverflow.com/questions/3899852/finding-all-images-in-a-flowdocument
+        public static IEnumerable<Image> FindImages(FlowDocument document)
+        {
+            return document.Blocks.SelectMany(block => FindImages(block));
+        }
+
+        public static IEnumerable<Image> FindImages(Block block)
+        {
+            if (block is Table)
+            {
+                return ((Table)block).RowGroups
+                    .SelectMany(x => x.Rows)
+                    .SelectMany(x => x.Cells)
+                    .SelectMany(x => x.Blocks)
+                    .SelectMany(innerBlock => FindImages(innerBlock));
+            }
+            if (block is Paragraph)
+            {
+                return ((Paragraph)block).Inlines
+                    .OfType<InlineUIContainer>()
+                    .Where(x => x.Child is Image)
+                    .Select(x => x.Child as Image);
+            }
+            if (block is BlockUIContainer)
+            {
+                Image i = ((BlockUIContainer)block).Child as Image;
+                return i == null
+                            ? new List<Image>()
+                            : new List<Image>(new[] { i });
+            }
+            if (block is List)
+            {
+                return ((List)block).ListItems.SelectMany(listItem => listItem
+                                                                      .Blocks
+                                                                      .SelectMany(innerBlock => FindImages(innerBlock)));
+            }
+            throw new InvalidOperationException("Unknown block type: " + block.GetType());
+        }*/
+        #endregion
     }
 }
