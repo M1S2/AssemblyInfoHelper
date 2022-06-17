22cf9c400e6444d56a3fb64ea8e85ad04d578605
Markus Scheich <markus.scheich@gmx.de>
2022-06-17 18:20:00 +0200

    Corrected BuildTime property by showing the CreationDate of the .exe file instead of the linker time from the .exe header

--- AssemblyInfoHelper/AssemblyInfoHelperClass.cs
+++ AssemblyInfoHelper/AssemblyInfoHelperClass.cs
@@ -135,36 +135,6 @@ namespace AssemblyInfoHelper
         //********************************************************************************************************************************************************************
 
         /// <summary>
-        /// Get the time of the last build of the assembly.
-        /// </summary>
-        /// <param name="assembly">Assembly from which to get the last linker time</param>
-        /// <returns>last build time</returns>
-        /// see: https://stackoverflow.com/questions/1600962/displaying-the-build-date?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
-        public static DateTime GetLinkerTimeFromAssembly(Assembly assembly)
-        {
-            var filePath = assembly.Location;
-            const int c_PeHeaderOffset = 60;
-            const int c_LinkerTimestampOffset = 8;
-
-            var buffer = new byte[2048];
-
-            using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
-                stream.Read(buffer, 0, 2048);
-
-            var offset = BitConverter.ToInt32(buffer, c_PeHeaderOffset);
-            var secondsSince1970 = BitConverter.ToInt32(buffer, offset + c_LinkerTimestampOffset);
-            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-
-            var linkTimeUtc = epoch.AddSeconds(secondsSince1970);
-
-            var localTime = TimeZoneInfo.ConvertTimeFromUtc(linkTimeUtc, TimeZoneInfo.Local);
-
-            return localTime;
-        }
-
-        //********************************************************************************************************************************************************************
-
-        /// <summary>
         /// Assembly title attribute
         /// </summary>
         public static string AssemblyTitle => GetAttributeFromAssembly(Assembly.GetEntryAssembly(), AssemblyAttributeTypes.TITLE);
@@ -245,7 +215,7 @@ namespace AssemblyInfoHelper
         /// Get the time of the last build of the assembly.
         /// </summary>
         /// <returns>last build time</returns>
-        public static DateTime AssemblyLinkerTime => GetLinkerTimeFromAssembly(Assembly.GetEntryAssembly());
+        public static DateTime AssemblyLinkerTime => File.GetCreationTime(Assembly.GetEntryAssembly().Location);
 
         //********************************************************************************************************************************************************************
 
